ccca80a382ce68e59f2477876fadfb867588a5f9
Quan Wang <quanwang452@gmail.com>
2022-04-11 00:45:44 -0400

    user can delete todo task

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def add():
     db.session.add(new_todo)
     db.session.commit()
     return redirect(url_for("home"))
-    
+
 @app.route("/update/<int:todo_id>")
 def update(todo_id):
     todo = Todo.query.filter_by(id=todo_id).first()
@@ -43,6 +43,12 @@ def update(todo_id):
     db.session.commit()
     return redirect(url_for("home"))
 
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("home"))
 
 if __name__ == "__main__":
     db.create_all()
